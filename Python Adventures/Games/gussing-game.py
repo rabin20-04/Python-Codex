5d51ef321584e382b5447d71b00f331afce00db3
Rabin <poudelrabin2004@gmail.com>
2024-09-26 15:27:38 +0545

    g

--- Python Adventures/Games/gussing-game.py
+++ Python Adventures/Games/gussing-game.py
@@ -10,7 +10,12 @@ def guess_game():
     while True:
         try:
             num = random.randint(2, 10)
-            a = int(input("Guess the Target: "))
+            a = int(input("Guess the Target between 2 to 10 : "))
+            if a < 2 or a > 10:
+                clear_screen()
+                print("Hey Buddy You entered {} ".format(a))
+                print("Guess the number between 2 to 10!")
+                continue
             clear_screen()
             if isinstance(a, int):
                 if a == num:
@@ -19,9 +24,10 @@ def guess_game():
                 else:
                     print("Nope Try again! \nThe target was {}".format(num))
         except ValueError:
-            print("HUh that was not a number :( \n Enter a integer!")
+            clear_screen()
+            print("HUh that was not a number :( \n Enter an integer!")
 
-            print('You Entered {} \nThe target was "{}"'.format(a, num))
+            print('The target was "{}"'.format(num))
 
 
 guess_game()
