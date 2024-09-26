7902f8073e343ef0e6ab6acf9991a862ca5c8803
Rabin <poudelrabin2004@gmail.com>
2024-09-26 16:25:53 +0545

    gg

--- Python Adventures/Games/gussing-game.py
+++ Python Adventures/Games/gussing-game.py
@@ -7,6 +7,9 @@ def clear_screen():
 
 
 def guess_game():
+    print(
+        "Welcome to the Guessing Game \nWe have a random number and you have to guess it !\nNew number will be generated each time you guess Wrong\nLet's start the game "
+    )
     while True:
         try:
             num = random.randint(2, 10)
@@ -30,4 +33,49 @@ def guess_game():
             print('The target was "{}"'.format(num))
 
 
-guess_game()
+def steady_random():
+    print(
+        "Welcome to the Guessing Game \nWe have a random number and you have to guess it !\nLet's start the game "
+    )
+    c = random.randint(2, 20)
+    while True:
+        try:
+            print(c)
+            a = int(input("Guess the Target between 2 to 20 : "))
+            if a < 2 or a > 20:
+                clear_screen()
+                print("Hey Buddy You entered {} ".format(a))
+                print("Guess the number between 2 to 20!")
+                continue
+            if isinstance(a, int):
+                print("You entered : ", a)
+                if a == c:
+                    print("Amazing You guessed the target")
+                    break
+
+                elif a < c:
+                    print("Ah Your input was Low!\nIncrease the value")
+                else:
+                    print("Oh Too High! \nDecrease your values")
+
+        except ValueError:
+            clear_screen()
+            print("HUh that was not a number :( \n Enter an integer!")
+
+
+try:
+    b = int(
+        input(
+            "Which game do you want to play? \n 1)Steady guessing game \n 2)Always random\n "
+        )
+    )
+    if isinstance(b, int):
+        clear_screen()
+        if b == 1:
+            steady_random()
+            clear_screen()
+        elif b == 2:
+            guess_game()
+            clear_screen()
+except ValueError:
+    print("Not a valid choice!")
