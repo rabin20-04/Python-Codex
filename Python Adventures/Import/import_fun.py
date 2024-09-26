693e76351500254ab23a8618355d98f5bf5d6aa0
Rabin <poudelrabin2004@gmail.com>
2024-09-26 17:41:50 +0545

    rm import

--- Python Adventures/Import/import_fun.py
+++
@@ -1,2 +0,0 @@
-import antigravity  # noqa: F401
-# noqa: F401 is a comment used to instruct the Python linter (a tool that checks your code for potential errors) to ignore a specific warning or error on the line where it appears.
