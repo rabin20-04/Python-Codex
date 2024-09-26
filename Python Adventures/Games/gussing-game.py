8895ca029ef27db2f1d88a08031a0db6511db3bd
Rabin <poudelrabin2004@gmail.com>
2024-09-26 17:11:14 +0545

    gg

--- Python Adventures/Games/gussing-game.py
+++ Python Adventures/Games/gussing-game.py
@@ -8,84 +8,78 @@ def clear_screen():
 
 def guess_game():
     print(
-        "Welcome to the Guessing Game \nWe have a random number and you have to guess it !\nNew number will be generated each time you guess Wrong\nLet's start the game "
+        "Welcome to the Guessing Game \nWe have a random number and you have to guess it!\nNew number will be generated each time you guess wrong.\nLet's start the game!"
     )
     while True:
         try:
-            num = random.randint(2, 10)
-            a = int(input("Guess the Target between 2 to 10 : "))
-            if a < 2 or a > 10:
+            target_number = random.randint(2, 10)
+            user_guess = int(input("Guess the target between 2 to 10: "))
+            if user_guess < 2 or user_guess > 10:
                 clear_screen()
-                print("Hey Buddy You entered {} ".format(a))
+                print("Hey Buddy, you entered {}.".format(user_guess))
                 print("Guess the number between 2 to 10!")
                 continue
-            if isinstance(a, int):
-                if a == num:
+            if isinstance(user_guess, int):
+                if user_guess == target_number:
                     clear_screen()
-
-                    print("Amazing You guessed the target")
+                    print("Amazing! You guessed the target.")
                     break
                 else:
                     clear_screen()
-                    print("Nope Try again! \nThe target was {}".format(num))
+                    print("Nope! Try again! \nThe target was {}".format(target_number))
         except ValueError:
             clear_screen()
-            print("HUh that was not a number :( \n Enter an integer!")
-
-            print('The target was "{}"'.format(num))
+            print("Huh, that was not a number :( \nEnter an integer!")
+            print('The target was "{}".'.format(target_number))
 
 
 def steady_random():
     print(
-        "Welcome to the Guessing Game \nWe have a random number and you have to guess it !\nLet's start the game "
+        "Welcome to the Steady Guessing Game \nWe have a random number and you have to guess it!\nLet's start the game!"
     )
-    c = random.randint(2, 20)
+    constant_number = random.randint(2, 20)
     while True:
         try:
-            print(c)
-            a = int(input("Guess the Target between 2 to 20 : "))
-            if a < 2 or a > 20:
+            user_guess = int(input("Guess the target between 2 to 20: "))
+            if user_guess < 2 or user_guess > 20:
                 clear_screen()
-                print("Hey Buddy You entered {} ".format(a))
+                print("Hey Buddy, you entered {}.".format(user_guess))
                 print("Guess the number between 2 to 20!")
                 continue
-            if isinstance(a, int):
-                print("You entered : ", a)
-                if a == c:
+            if isinstance(user_guess, int):
+                print("You entered: ", user_guess)
+                if user_guess == constant_number:
                     clear_screen()
-                    print("Amazing You guessed the target")
+                    print("Amazing! You guessed the target.")
                     break
-
-                elif a < c:
+                elif user_guess < constant_number:
                     clear_screen()
-
-                    print("Ah Your input was Low!\nIncrease the value")
+                    print("Ah, your input was low!\nIncrease the value.")
                 else:
                     clear_screen()
-
-                    print("Oh Too High! \nDecrease your values")
-
+                    print("Oh, too high! \nDecrease your values.")
         except ValueError:
             clear_screen()
-            print("HUh that was not a number :( \n Enter an integer!")
+            print("Huh, that was not a number :( \nEnter an integer!")
 
 
-b = 0
-while b != 3:
+menu_option = 0
+while menu_option != 3:
     print("\nWelcome to the main menu")
     try:
-        b = int(
+        menu_option = int(
             input(
-                "Which game do you want to play? \n 1)Steady guessing game \n 2)Always random\n 3)Exit\n  "
+                "Which game do you want to play? \n1) Steady Guessing Game \n2) Always Random\n3) Exit\n  "
             )
         )
         clear_screen()
-        if isinstance(b, int):
-            if b == 1:
+        if isinstance(menu_option, int):
+            if menu_option == 1:
                 steady_random()
-            elif b == 2:
+            elif menu_option == 2:
                 guess_game()
-            elif b == 3:
+            elif menu_option == 3:
                 print("Thank you for playing! Goodbye!")
     except ValueError:
+        clear_screen()
         print("Not a valid choice!")
